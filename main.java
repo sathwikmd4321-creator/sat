65a539eadb7dea38678d9ce24ea6c70d136e2d4a
SATHWIK M.D <sathwikmd4321@gmail.com>
2026-08-12 10:35:17 +0530

    SUM

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+        int arr[] = { 40, 100, 5, 60, 10, 80 };
+        int sum = 0;
+        for (int i = 0; i <= arr.length - 1; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.print(sum);
+
+    }
+}
